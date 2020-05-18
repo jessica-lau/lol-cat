7653908a22e434350b18a5e88bb637c1d6c76bfe
jessica-lau <jessica.lau2@gmail.com>
2020-05-17 20:08:34 -0400

    add event listener to party button

--- js/lol_dog.js
+++ js/lol_dog.js
@@ -7,49 +7,49 @@ var nap = 15;
 var party = 21;
 
 
-var updateClock = function() {
+var updateClock = function () {
 
-var messageText;
-var timeEventJS = document.getElementById("timeEvent");
-var loldog = document.getElementById("loldog");
-console.log(loldog);
-var image = "./img/dog-chilling.jpg";
+	var messageText;
+	var timeEventJS = document.getElementById("timeEvent");
+	var loldog = document.getElementById("loldog");
+	console.log(loldog);
+	var image = "./img/dog-chilling.jpg";
 
-if (time == wakeup) {
-	image = "./img/dog-wakeup.jpg";
-	messageText = "Time to wake up!";
-}
-else if (time == lunch) {
-	image = "./img/dog-lunch.jpg";
-	messageText = "Time to eat some yummy lunch.";
-}
-else if (time == nap) {
-	image = "./img/dog-nap.jpg";
-	messageText = "Let's take a nap.";
-}
-else if (time == party) {
-	image = "./img/dog-party.jpg";
-	messageText = "I'm going to par-taaay!";
-}
-else if (time < noon) {
-	image = "./img/dog-chilling.jpg";
-	messageText = "Good morning sunshine!";
-}
-else if (time > evening) {
-	image = "./img/dog-chilling.jpg";
-	messageText = "Good evening! It's dinner time!";
-}
-else {
-	image = "./img/dog-chilling.jpg";
-	messageText = "Good afternoon!";
-}
+	if (time == wakeup) {
+		image = "./img/dog-wakeup.jpg";
+		messageText = "Time to wake up!";
+	}
+	else if (time == lunch) {
+		image = "./img/dog-lunch.jpg";
+		messageText = "Time to eat some yummy lunch.";
+	}
+	else if (time == nap) {
+		image = "./img/dog-nap.jpg";
+		messageText = "Let's take a nap.";
+	}
+	else if (time == party) {
+		image = "./img/dog-party.jpg";
+		messageText = "I'm going to par-taaay!";
+	}
+	else if (time < noon) {
+		image = "./img/dog-chilling.jpg";
+		messageText = "Good morning sunshine!";
+	}
+	else if (time > evening) {
+		image = "./img/dog-chilling.jpg";
+		messageText = "Good evening! It's dinner time!";
+	}
+	else {
+		image = "./img/dog-chilling.jpg";
+		messageText = "Good afternoon!";
+	}
 
-console.log(messageText);
-console.log(timeEventJS);
-timeEventJS.innerText = messageText;
-loldog.src = image;
+	console.log(messageText);
+	console.log(timeEventJS);
+	timeEventJS.innerText = messageText;
+	loldog.src = image;
 
-showCurrentTime();
+	showCurrentTime();
 };
 
 var showCurrentTime = function () {
@@ -91,3 +91,32 @@ var showCurrentTime = function () {
 var oneSecond = 1000;
 updateClock();
 setInterval(updateClock, oneSecond);
+
+var partyButton = document.getElementById("partyTimeButton");
+
+// set isPartyTime to false
+var isPartyTime = false;
+
+var partyEvent = function () {
+
+	//check isPartyTime
+	if (isPartyTime === false) {
+		// if isPartyTime is false, change it to true 
+		//so we know the button has been clicked
+		isPartyTime = true;
+		// set time to partyTime so the lolCat clock 
+		//image and message update to the partyTime image and message
+		time = party;
+		partyButton.innerText("Party Over");
+
+	} else {
+		// if isPartyTime is true, change it to false to end the party
+		isPartyTime = false;
+		// set time back to the current time
+		time = new Date().getHours();
+		partyButton.innerText("Party Time!");
+	}
+
+};
+
+partyButton.addEventListener("click",partyEvent);
